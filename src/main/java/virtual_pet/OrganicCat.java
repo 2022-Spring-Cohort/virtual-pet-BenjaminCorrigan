e932e54b61147312dc2e3d0c211628475bc5458d
BenjaminCorrigan <corriganbenjamin>
2022-02-08 16:41:39 -0500

    merge commit

---
+++ src/main/java/virtual_pet/OrganicCat.java
@@ -0,0 +1,13 @@
+package virtual_pet;
+
+public class OrganicCat extends Cat{
+
+    public OrganicCat(String name, String furColor, int hunger, int thirst, int boredom) {
+        super(name, furColor, hunger, thirst, boredom);
+
+    }
+    public void cleanCage(){
+
+    }
+
+}
